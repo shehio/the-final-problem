fb01221659572871d9f90b4d32276c30305b15f7
Shehab Yasser <shehabyasser@gmail.com>
2023-12-30 23:31:36 +0000

    Modified barrier.sh

--- raspberrypi/barrier.sh
+++ raspberrypi/barrier.sh
@@ -1,1 +1,8 @@
 sudo apt install barrier
+
+# Create a certificate
+cd /home/$(whoami)/.local/share/barrier/SSL/
+mkdir -p Fingerprints
+openssl req -x509 -nodes -days 365 -subj /CN=barrier -newkey rsa:4096 -keyout Barrier.pem -out Barrier.pem
+openssl x509 -fingerprint -sha256 -noout -in Barrier.pem > Fingerprints/Local.txt
+sed -e "s/.*=/v2:sha256:/" -i Fingerprints/Local.txt
