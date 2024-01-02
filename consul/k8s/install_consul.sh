8ee952126e269f06ef5380d6543b65c43e230796
Shehab Yasser <shehabyasser@gmail.com>
2024-01-01 20:47:39 -0500

    Update install_consul.sh

--- consul/k8s/install_consul.sh
+++ consul/k8s/install_consul.sh
@@ -1,3 +1,4 @@
 helm repo add hashicorp https://helm.releases.hashicorp.com
-helm install consul-release hashicorp/consul -version 1.0.0 --values consul-values.yaml --set global.datacenter=eks 
+helm install consul-release hashicorp/consul -version 1.0.0 --values consul-values.yaml --set global.datacenter=eks
 kubectl get pods
+kubectl get all
