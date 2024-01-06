099fb436d229682363c9c5a0c4473d37f95ba01f
Shehab Yasser <shehabyasser@gmail.com>
2024-01-06 23:16:49 +0000

    Modified raspberrypi/kvm/README.md

--- raspberrypi/kvm/barrier.sh
+++ raspberrypi/kvm/barrier.sh
@@ -1,3 +1,4 @@
+sudo apt update
 sudo apt install barrier
 
 # Create a certificate
@@ -6,7 +7,3 @@ mkdir -p Fingerprints
 openssl req -x509 -nodes -days 365 -subj /CN=barrier -newkey rsa:4096 -keyout Barrier.pem -out Barrier.pem
 openssl x509 -fingerprint -sha256 -noout -in Barrier.pem > Fingerprints/Local.txt
 sed -e "s/.*=/v2:sha256:/" -i Fingerprints/Local.txt
-
-# Manually disable Wayland and enable X11
-> sudo raspi-config
-Choose 6 Advanced Options > A6 Wayland > W1 X11
