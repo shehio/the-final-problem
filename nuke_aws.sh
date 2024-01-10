aed4ae74e4685132a42e192395ff5a308d1a7075
shehio <shehabyasser@gmail.com>
2024-01-10 18:53:40 -0500

    Updated nuke_aws.sh

--- nuke_aws.sh
+++ nuke_aws.sh
@@ -1,4 +1,4 @@
-wget -c https://github.com/rebuy-de/aws-nuke/releases/download/v2.25.0/aws-nuke-v2.25.0-linux-amd64.tar.gz
-tar -xvf aws-nuke-v2.25.0-linux-amd64.tar.gz 
-mv aws-nuke-v2.25.0-linux-amd64 aws-nuke
+wget -c https://github.com/rebuy-de/aws-nuke/releases/download/v2.25.0/aws-nuke-v2.25.0-linux-arm64.tar.gz
+tar -xvf aws-nuke-v2.25.0-linux-arm64.tar.gz
+mv aws-nuke-v2.25.0-linux-arm64 aws-nuke
 sudo mv aws-nuke /usr/local/bin/aws-nuke
