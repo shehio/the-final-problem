d6874886794bda3f00ed0078eec2d642fa73d27a
shehio <shehabyasser@gmail.com>
2024-01-14 08:19:27 -0500

    Added more deps to pi_setup.sh

--- setup/raspberrypi os/pi_setup.sh
+++ setup/raspberrypi os/pi_setup.sh
@@ -17,5 +17,8 @@ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scrip
 chmod 700 get_helm.sh
 ./get_helm.sh
 
+sudo apt install redis-tools
+sudo apt install zookeeper
+
 sudo apt install code -y
 sudo apt install gitk -y
