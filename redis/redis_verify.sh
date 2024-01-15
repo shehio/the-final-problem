62260a1dc51995cf5d7e7225c34e50b0f75a5d09
shehio <shehabyasser@gmail.com>
2024-01-15 14:13:28 -0500

    Modified redis_verify.sh

--- redis/redis_verify.sh
+++ redis/redis_verify.sh
@@ -1,4 +1,9 @@
 kubectl get pods
 kubectl get service
 
+export SERVICE_IP=$(kubectl get svc --namespace default my-redis2-redis-cluster-0-svc --template "{{ range (index .status.loadBalancer.ingress 0) }}{{ . }}{{ end }}")
+
+redis-cli -c -h $SERVICE_IP -p 6379 -a $REDIS_P
+export REDIS_PASSWORD=$(kubectl get secret --namespace "default" my-redis2-redis-cluster -o jsonpath="{.data.redis-password}" | base64 -d)
+
 python redis-write-read.py
