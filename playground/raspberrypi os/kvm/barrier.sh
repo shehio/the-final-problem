4593c27fa23f005de7a5b26f1fca62dd91d5b59c
Shehab Yasser <shehabyasser@gmail.com>
2024-01-20 22:06:02 -0500

    Update barrier.sh

--- playground/raspberrypi os/kvm/barrier.sh
+++ playground/raspberrypi os/kvm/barrier.sh
@@ -1,7 +1,8 @@
 sudo apt update
-sudo apt install barrier
+sudo apt install barrier -y
 
 # Create a certificate
+mkdir -p /home/$(whoami)/.local/share/barrier/SSL/
 cd /home/$(whoami)/.local/share/barrier/SSL/
 mkdir -p Fingerprints
 openssl req -x509 -nodes -days 365 -subj /CN=barrier -newkey rsa:4096 -keyout Barrier.pem -out Barrier.pem
