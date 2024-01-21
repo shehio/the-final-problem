2459589d40f62e5c8595c1893b513cd564e46665
shehio <shehabyasser@gmail.com>
2024-01-21 08:26:19 -0500

    Created install_tailscale.sh

---
+++ playground/raspberrypi-os/install_tailscale.sh
@@ -0,0 +1,9 @@
+curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+
+sudo apt update
+sudo apt install tailscale
+
+sudo tailscale up
+
+tailscale ip -4
