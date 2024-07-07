4c586de28b9e33a56491f7b14b18799a270731f7
Shehab Yasser <shehabyasser@gmail.com>
2024-07-07 13:40:37 -0400

    Renamed scripts

--- cluster/components/redis/scripts/cluster_verification.sh
+++ cluster/components/redis/scripts/cluster_verification.sh
@@ -3,5 +3,3 @@ kubectl get service
 
 export REDIS_PASSWORD=$(kubectl get secret --namespace default my-redis -o jsonpath="{.data.redis-password}" | base64 -d)
 redis-cli -c -h $SERVICE_IP -p 6379 -a $REDIS_PASSWORD
-
-python redis-write-read.py
