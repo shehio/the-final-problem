4048509a346ff0003e4df7106b877f3a4833d7d2
Shehab Yasser <shehabyasser@gmail.com>
2024-07-07 13:38:24 -0400

    Updated redis-write-read.py

--- cluster/components/redis/redis-write-read.py
+++ cluster/components/redis/redis-write-read.py
@@ -1,11 +1,10 @@
 import redis
 
-# todo: Get the service ip from env variables
-redis_host = ' ' # fill in the host here
+redis_host = '127.0.0.1' # Assuming port forwarding: kubectl port-forward --namespace default svc/my-redis-master 6379:6379
 redis_port = 6379
 
 # todo: Get the password from env variables
-client = redis.Redis(host=redis_host, port=redis_port, password=' ') # fill in the password here
+client = redis.Redis(host=redis_host, port=redis_port, password='') # fill in the password here
 
 client.set('hello', 'world')
 
