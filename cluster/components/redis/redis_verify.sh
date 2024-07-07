42c011486093982afb67537b8dd0216e84c26eeb
Shehab Yasser <shehabyasser@gmail.com>
2024-07-07 13:38:05 -0400

    Updated redis scripts

--- cluster/components/redis/redis_verify.sh
+++ cluster/components/redis/redis_verify.sh
@@ -1,8 +1,7 @@
 kubectl get pods
 kubectl get service
 
-export SERVICE_IP=$(kubectl get svc --namespace default my-redis2-redis-cluster-0-svc --template "{{ range (index .status.loadBalancer.ingress 0) }}{{ . }}{{ end }}")
-export REDIS_PASSWORD=$(kubectl get secret --namespace "default" my-redis2-redis-cluster -o jsonpath="{.data.redis-password}" | base64 -d)
+export REDIS_PASSWORD=$(kubectl get secret --namespace default my-redis -o jsonpath="{.data.redis-password}" | base64 -d)
 redis-cli -c -h $SERVICE_IP -p 6379 -a $REDIS_PASSWORD
 
 python redis-write-read.py
