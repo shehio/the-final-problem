7e13339af783f43e75877ef93b95354c0b01d6f0
shehio <shehabyasser@gmail.com>
2026-03-21 03:04:16 +0200

    fix: Undefined variable $SERVICE_IP
    Fixes CODE-282

--- cluster/components/redis/scripts/cluster_verification.sh
+++ cluster/components/redis/scripts/cluster_verification.sh
@@ -2,4 +2,5 @@ kubectl get pods
 kubectl get service
 
 export REDIS_PASSWORD=$(kubectl get secret --namespace default my-redis -o jsonpath="{.data.redis-password}" | base64 -d)
+export SERVICE_IP=$(kubectl get svc my-redis-master -o jsonpath='{.spec.clusterIP}')
 redis-cli -c -h $SERVICE_IP -p 6379 -a $REDIS_PASSWORD
